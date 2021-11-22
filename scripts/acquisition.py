f975b1d39eb08b4e7ed147a601bb1ec2377a763c
Leonhard Horstmeyer <leo.h@hotmail.de>
2021-11-22 13:57:58 +0100

    added some docstrings to the script files

--- scripts/acquisition.py
+++ scripts/acquisition.py
@@ -7,7 +7,9 @@ import requests
 
 
 class Acquisition():
+    """Class that handles data acquisition and retrieval."""
 
+    ## Some common exceptions
     noSuchTableException = Exception('No such data table exists. Please see the ./data directory for allowed table names and either use one of the existing names or add this table to the list together with the api rquired to fetch it.')
     noSuchInfoException = Exception('No such information exists for this table. Please see the ./data directory for allowed information keys and either use one of the existing keys or add this new key to the list together with the required value.')
     cannotGetTableException = Exception('We cannot retrieve the data from this table, either because the api request failed or because you have requested not to get the data in the first place (check attributes of the methods you used).')
@@ -15,6 +17,19 @@ class Acquisition():
 
     @classmethod
     def _getInfoFromTableName(cls, whichData, info="filename"):
+        """method to obtain information about the data table
+
+        Args:
+            whichData (str): name of the data table.
+            info (str, optional): key of the value that we want to retrieve. Defaults to "filename".
+
+        Raises:
+            cls.noSuchTableException: The table doesnt exist.
+            cls.noSuchInfoException: The key doesnt exist.
+
+        Returns:
+            str or int: the value of the requested information.
+        """
         if whichData not in CONFIG.DataSources:
             raise cls.noSuchTableException
         if info=="filename":
@@ -31,10 +46,26 @@ class Acquisition():
                 raise cls.noSuchInfoException
             return CONFIG.DataSources[whichData][info]
 
+
     @classmethod
     def readDataLocally(cls, whichData="cg_asset_platforms", downloadIfMissing=True, forceUpdate=False):
+        """Read data from a given table or fetch it, if its not there.
+
+        Args:
+            whichData (str, optional): selected data table. Defaults to "cg_asset_platforms".
+            downloadIfMissing (bool, optional): [if data table cant be found, should it be fetched? Defaults to True.
+            forceUpdate (bool, optional): update data irrespective of its current status. Defaults to False.
+
+        Raises:
+            cls.cannotGetTableException: cannot get table
+
+        Returns:
+            dict or list: return data from jsonized data-table. 
+        """
+
         completeFilePath = cls._getInfoFromTableName(whichData=whichData, info="filename")
         isFileFlag = os.path.isfile(completeFilePath)
+        
         if not isFileFlag:
             if downloadIfMissing:
                 status = cls.updateDataLocally(whichData=whichData)
@@ -53,8 +84,18 @@ class Acquisition():
 
         return returnData
 
+
     @classmethod
     def updateDataLocally(cls, whichData="cg_asset_platforms"):
+        """Updates data by writing into the respective data table (check data_sources.json for reference).
+
+        Args:
+            whichData (str, optional): name of the data table. Defaults to "cg_asset_platforms".
+
+        Returns:
+            int: status code for the request
+        """
+
         completeFilePath = cls._getInfoFromTableName(whichData=whichData, info="filename")
         dataAPI = cls._getInfoFromTableName(whichData=whichData, info="api")
         req = requests.get(dataAPI)
--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import json
 
 class CONFIG():
+    """Configuration class for the scripts"""
+    
     ConfigPath = os.path.dirname(os.path.realpath(__file__))
     DataDirectory = os.path.join(ConfigPath, '..', 'data')
     with open(os.path.join(DataDirectory, "data_sources.json"), "r") as file:
